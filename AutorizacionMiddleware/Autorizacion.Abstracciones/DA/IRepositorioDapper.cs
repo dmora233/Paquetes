c5a16fd9185327b55bae497294308b2a678bf470
dmora233 <148721494+dmora233@users.noreply.github.com>
2025-03-10 21:57:49 -0600

    Implementacion paquetes

---
+++ AutorizacionMiddleware/Autorizacion.Abstracciones/DA/IRepositorioDapper.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+namespace Autorizacion.Abstracciones.DA
+{
+    public interface IRepositorioDapper
+    {
+        SqlConnection ObtenerRepositorioDapper();
+    }
+}
---
+++ AutorizacionMiddleware/Autorizacion.DA/Repositorios/RepositorioDapper.cs
@@ -0,0 +1,23 @@
+﻿using Autorizacion.Abstracciones.DA;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Autorizacion.DA.Repositorios
+{
+    public class RepositorioDapper : IRepositorioDapper
+    {
+        private readonly IConfiguration _configuration;
+        private SqlConnection _connection;
+
+        public RepositorioDapper(IConfiguration configuration, SqlConnection connection)
+        {
+            _configuration = configuration;
+            _connection = new SqlConnection(_configuration.GetConnectionString("BDSeguridad"));
+        }
+
+        public SqlConnection ObtenerRepositorioDapper()
+        {
+            return _connection;
+        }
+    }
+}
